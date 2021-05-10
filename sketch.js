91bfc2ebf99bb389a183e5183da32ae313cc230d
jaskaran <74704788+jaskaran365@users.noreply.github.com>
2021-05-10 17:07:23 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -75,6 +75,9 @@ function draw() {
   //Engine.update(engine);
   text(mouseX + ',' + mouseY, 10, 15);
   textSize(20);
+  fill("lightyellow");
+  text("Drag the polygon to destroy the blocks",300,30);
+  textSize(10);
   text("Press Space to get a second Chance to Play!!",650 ,350);
   ground.display();
   stand1.display();
